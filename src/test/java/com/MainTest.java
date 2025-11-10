17c81fd75d82e95e08441262229cfbc9a1c94e88
une_glace <une_glace@sjtu.edu.cn>
2025-11-10 14:08:06 +0800

    changeTest

--- src/test/java/com/MainTest.java
+++ src/test/java/com/MainTest.java
@@ -1,7 +1,6 @@
 package com;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 class MainTest {
@@ -10,7 +9,7 @@ class MainTest {
 	void test() {
 		Main main = new Main();
 		int result = main.add(1, 1);
-		assertEquals(1, result);
+		assertEquals(2, result);
 	}
 
 }
